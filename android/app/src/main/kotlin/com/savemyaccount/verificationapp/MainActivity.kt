221d7f1c4e6a358be17ade374636f206868a6ba0
Jaky Astik <12696342+jakyastik@users.noreply.github.com>
2026-06-09 13:39:43 +0000

    Deploy pristine lightweight app blueprint

--- android/app/src/main/kotlin/com/savemyaccount/verificationapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/savemyaccount/verificationapp/MainActivity.kt
@@ -1,6 +1,3 @@
 package com.savemyaccount.verificationapp
-
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,2 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text(
-          'Verification App Active',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-  ));
-}
+void main() => runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('Verification Active', style: TextStyle(fontSize: 24))))));
